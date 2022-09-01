bfe32f154ca30f9a1a48e761f75237cb99c145fc
Ellian <dbsrn0125@naver.com>
2022-09-02 06:49:55 +0900

    abc

---
+++ PolarStar/Assets/KYG/KYG_Scripts/StarGuide.cs
@@ -0,0 +1,137 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StarGuide : MonoBehaviour
+{
+    public enum GuideState
+    {
+        guideStart,
+        state1,
+        state2,
+        state3,
+        state4,
+        state5
+    }
+    public string startText = "환영합니다!!";
+    public string S_1Text = "1번 키를 눌러 찾고싶은 별자리를 말해주세요!";
+    public string S_2Text = "음성 인식 중...";
+    public string S_3Text = "별자리를 찾았습니다! 고개를 들어 하늘을 바라보세요!";
+    public string S_4Text = "오디오 재생중";
+    public string S_5Text = "2번 키를 눌러 별자리와 사진을 찍어보세요!";
+    public Image GuideBar;
+    public Text guideText;
+    public GuideState guideState;
+    public static StarGuide Instance;
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        guideState = GuideState.guideStart;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StateMachine();
+    }
+
+    private void StateMachine()
+    {
+        switch(guideState)
+        {
+            case GuideState.guideStart:
+                guideStart();
+                break;
+            case GuideState.state1:
+                state1();
+                break;
+            case GuideState.state2:
+                state2();
+                break;
+            case GuideState.state3:
+                state3();
+                break;
+            case GuideState.state4:
+                state4();
+                break;
+            case GuideState.state5:
+                state5();
+                break;
+        }
+    }
+    float currentTime = 0;
+    float guideStartTime = 5f;
+    private void guideStart()
+    {
+        StartCoroutine("FadeOut");
+        guideText.text = startText;
+        currentTime += Time.deltaTime;
+        if (currentTime > guideStartTime)
+        {
+            StopAllCoroutines();
+            StartCoroutine("FadeIn");
+            guideState = GuideState.state1;
+            currentTime = 0;
+        }
+    }
+
+    private void state1()
+    {
+        
+        StartCoroutine("FadeOut");
+        guideText.text = S_1Text;
+        StartCoroutine("FadeIn");
+        guideState = GuideState.state2;
+    }
+
+    private void state2()
+    {
+        guideText.text = S_2Text;
+        //guideState = GuideState.state3;
+    }
+
+    private void state3()
+    {
+        guideText.text = S_3Text;
+        guideState = GuideState.state4;
+    }
+
+    private void state4()
+    {
+        guideText.text = S_4Text;
+        guideState = GuideState.state5;
+    }
+
+    private void state5()
+    {
+        guideText.text = S_5Text;
+    }
+
+    public IEnumerator FadeOut()
+    {
+        for (float i = 0; i <= 0.5f; i += Time.deltaTime/7)
+        {
+            GuideBar.color = new Color(0, 0, 0, i);
+            guideText.color = new Color(1, 1, 1, 2 * i);
+            yield return null;
+        }
+    }
+    public IEnumerator FadeIn()
+    {
+        for (float i = 0.5f; i >= 0; i -= Time.deltaTime/7)
+        {
+            GuideBar.color = new Color(0, 0, 0, i);
+            guideText.color = new Color(1, 1, 1, 2 * i);
+            yield return null;
+        }
+    }
+}
