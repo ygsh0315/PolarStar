ec006be03d2f918cfe94155088677711835a2368
Ellian <dbsrn0125@naver.com>
2022-09-02 03:03:38 +0900

    abc

--- PolarStar/Assets/KYG/KYG_Scripts/StarRay.cs
+++ PolarStar/Assets/KYG/KYG_Scripts/StarRay.cs
@@ -6,26 +6,68 @@ public class StarRay : MonoBehaviour
     Ray starRay;
     RaycastHit starRayHitInfo;
     LayerMask layerMask;
-    GameObject Player;
+    public GameObject Player;
+    public GameObject Earth;
     float distance;
-    bool isSelected = false;
+    bool isSelected = true;
+
+    public static StarRay instance;
+
+    private void Awake()
+    {
+        if (!instance)
+        {
+            instance = this;
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        Player = GameObject.Find("Player");
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        layerMask = LayerMask.GetMask("Earth");
-        starRay = new Ray(transform.position, Vector3.zero);
-        if(isSelected == true)
-        {
-            Physics.Raycast(starRay, out starRayHitInfo, 1000 ,layerMask);
-            Debug.DrawRay(starRay.origin, (Vector3.zero - transform.position).normalized * 1000, Color.red);
+        ////layerMask = LayerMask.GetMask("Earth");
+        //starRay = new Ray(transform.position, Earth.transform.position - transform.position);
+        //Vector3 dir = Vector3.zero - transform.position;
 
-        }
+        //if(Physics.Raycast(starRay, out starRayHitInfo, 1000))
+        //{
+        //    if (Input.GetKeyDown(KeyCode.T))
+        //    {
+        //        Debug.DrawRay(starRay.origin, (Earth.transform.position - transform.position).normalized * 1000, Color.red);
+        //        print(starRayHitInfo.point);
+        //        Player.transform.position = starRayHitInfo.point;
+
+        //        //Player.transform.up
+        //    }
+        //}
         
+        
+    }
+    public void transfort(GameObject stars)
+    {
+        Transform trans = stars.transform.GetChild(1).transform;
+        print(trans);
+        if (!trans) return;
+
+        //layerMask = LayerMask.GetMask("Earth");
+        starRay = new Ray(trans.position, Earth.transform.position - trans.position);
+        Vector3 dir = Vector3.zero - trans.position;
+
+        if (Physics.Raycast(starRay, out starRayHitInfo, 1000))
+        {
+            //if (Input.GetKeyDown(KeyCode.T))
+            //{
+                Debug.DrawRay(starRay.origin, (Earth.transform.position - trans.position).normalized * 1000, Color.red);
+                print(starRayHitInfo.point);
+                Player.transform.position = starRayHitInfo.point;
+
+                //Player.transform.up
+            //}
+        }
     }
 }
