4a6a084761f569156dcf9847ab2e9ca7b85fb580
Kang-JiHyeon <65221707+Kang-JiHyeon@users.noreply.github.com>
2022-09-01 14:34:59 +0900

    Update KJH_Star.cs

--- PolarStar/Assets/KJH/Scripts/KJH_Star.cs
+++ PolarStar/Assets/KJH/Scripts/KJH_Star.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// 구 위에 랜덤 위치에 랜덤 크기로 
 public class KJH_Star : MonoBehaviour
 {
     // Start is called before the first frame update
